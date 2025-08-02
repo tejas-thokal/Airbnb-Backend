35a124050fdae7f23ec4afa4717f8b181b4f9fda
tejas-thokal <tejasthokal2305@gmail.com>
2025-08-02 18:12:16 +0530

    change database

--- Backend/db.js
+++ Backend/db.js
@@ -3,14 +3,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
